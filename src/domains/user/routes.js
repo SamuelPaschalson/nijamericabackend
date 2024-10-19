e8bbba703a47cffbfeaa5aff08376f27c907d21d
SamuelPaschalson <109558179+SamuelPaschalson@users.noreply.github.com>
2024-10-19 13:14:13 +0100

    Update routes.js

--- src/domains/user/routes.js
+++ src/domains/user/routes.js
@@ -2,10 +2,11 @@ const userController = require("./controller");
 // const nodemailer = require("nodemailer");
 const express = require("express");
 const router = express.Router();
- 
+
 // create a new user
 router.post("/register", userController.user_signup);
 router.post("/login", userController.user_login);
 router.put("/:user_id", userController.update_user);
+router.put("/change-password/:user_id", userController.change_password);
 
 module.exports = router;
