2196284a49deb4bcb0232e4f28dd66a2e7670828
SamuelPaschalson <109558179+SamuelPaschalson@users.noreply.github.com>
2024-10-19 02:45:34 +0100

    Update model.js

--- src/domains/business/model.js
+++ src/domains/business/model.js
@@ -1,45 +1,70 @@
 const mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema({
-	user: {
-		type: String,
-		required: true,
-	},
-	rating: {
-		type: Number,
-		required: true,
-		min: 1,
-		max: 5,
-	},
-	comment: {
-		type: String,
-		required: true,
-	},
+  user: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Product schema
+const productSchema = new mongoose.Schema({
+  product_name: {
+    type: String,
+    required: true,
+  },
+  product_description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+  },
+  reviews: [reviewSchema], // Product reviews
 });
 
 const businessSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	address: {
-		type: String,
-		required: true,
-	},
-	phone: {
-		type: String,
-		required: true,
-	},
-	website: {
-		type: String,
-	},
-	category: {
-		type: String,
-	},
-	location: {
-		type: String,
-	},
-	// reviews: [reviewSchema],
+  business_name: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  business_owner: {
+    type: String,
+    required: true,
+  },
+  business_description: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  business_id: {
+    type: String,
+    required: true,
+  },
+  reviews: [reviewSchema], // Business reviews
+  products: [productSchema],
 });
 
 module.exports = mongoose.model("Business", businessSchema, "business");
