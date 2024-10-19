380d61de0c0257d0f49efacbf86599d69afecc34
SamuelPaschalson <109558179+SamuelPaschalson@users.noreply.github.com>
2024-10-19 08:24:45 +0100

    Update routes.js

--- src/domains/business/routes.js
+++ src/domains/business/routes.js
@@ -4,7 +4,12 @@ const express = require("express");
 const router = express.Router();
 
 // create a new business
-router.get("/fetch-list", businessController.fetch_business);
+router.get("/fetch-business/:business_id", businessController.fetch_business);
 router.post("/add-business", businessController.list_business);
+router.get("/add-reviews", businessController.count);
+router.get("/reviews", businessController.count);
+router.get("/add-products", businessController.count);
+router.get("/products", businessController.count);
+router.get("/count", businessController.count);
 
 module.exports = router;
