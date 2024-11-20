019f1a25c46d536f9ad2f3ae4ebe81f2b66470e5
SamuelPaschalson <109558179+SamuelPaschalson@users.noreply.github.com>
2024-11-20 13:39:57 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -12,5 +12,6 @@ app.use(cors());
 app.use(bodyParser());
 app.use(routes);
 app.use(morgan("dev"));
-app.use(express.static('uploads'));
+// app.use(express.static('uploads'));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 module.exports = app;
